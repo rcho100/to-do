e53c2ad1986e10d83badf9d7c6128f5c0e0b729a
Richard Cho <richardcho100@gmail.com>
2020-09-29 23:59:42 -0400

    Use handleChange prop to handle change in checkbox

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,12 @@
 import React from "react"
 
-function TodoItem({item}) {
+function TodoItem({item, handleChange}) {
     return (
         <div className="todo-item">
-            <input
-                type="checkbox"
-                checked={item.completed}
-                onChange={console.log('changed')}
+            <input 
+                type="checkbox" 
+                checked={item.completed} 
+                onChange={() => handleChange(item.id)}
             />
             <p>{item.text}</p>
         </div>
