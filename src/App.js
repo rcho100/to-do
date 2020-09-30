679dec920d8c3658da98bbcf2d22a587d9cee479
Richard Cho <richardcho100@gmail.com>
2020-09-29 23:49:05 -0400

    Add handleChange function & pass to TodoItem as prop

--- src/App.js
+++ src/App.js
@@ -10,8 +10,25 @@ class App extends React.Component {
     }
   }
 
+  handleChange = (id) => {
+    this.setState((prevState) => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
+  }
+
   render () {
-    const todos = this.state.todos.map(item => <TodoItem key={item.id} item={item}todo/>)
+    const todos = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
     return (
       <div className="todo-list">
         {todos}
